8de6e75749714da290f179dbed167d60a331bd1c
EC2 Default User <ec2-user@ip-172-31-12-144.ap-northeast-1.compute.internal>
2025-08-05 11:36:09 +0000

    before RSpec test

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,13 @@
 Rails.application.routes.draw do
-  resources :lists
-  get 'lists/new'
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/edit'
-  get 'books/top'
-  get 'top' => 'homes#top'
-  get 'books' => 'books#top'
+  resources :books #7つのルートを自動作成　ただしtopは別で作る必要あり
+  resources :homes
+  get 'books/index'
+  get 'bookers' => 'homes#top'
+  get 'bookers/books' => 'books#index'
+  post 'bookers/books'=>'lists#create'
+  get 'books/:id' => 'books#show'
+  
+
+  
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
